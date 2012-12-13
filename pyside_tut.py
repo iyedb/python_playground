bd6f396ff9047bcfd86e492c3435eae715a2452b
Iyed <iyed.bennour@gmail.com>
2012-12-13 14:53:11 +0100

    Update pyside_tut.py

--- pyside_tut.py
+++ pyside_tut.py
@@ -11,18 +11,47 @@ app.exec_()
 sys.exit()
 """
 
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+	def closeEvent(self, event):
+		event.ignore()
+		self.hide()
+		
+		
 
-def quit():
-  sys.exit()
+def _quit():
+	app.quit()
+	qDebug('bye!')
 	
 app = QApplication(sys.argv)
 systrayico = QSystemTrayIcon(QIcon("icon18.gif"))
 systrayico.show()
-#systrayico.showMessage('hello', 'hello', msecs=2000)
+systrayico.showMessage('hello', 'hello', msecs=2000)
+
+def what(thereason):
+	print thereason
+	if thereason == QSystemTrayIcon.ActivationReason.Trigger:
+		main_window.show()
+		
 
 menu = QMenu()
 quitAction = menu.addAction('Quit')
-quitAction.triggered.connect(quit)
+quitAction.triggered.connect(_quit)
+systrayico.activated.connect(what)
+
 systrayico.setContextMenu(menu)
+
+scene = QGraphicsScene()
+scene.addText("Hello, world!")
+
+view = QGraphicsView(scene)
+
+
+main_window = MainWindow()
+main_window.setCentralWidget(view)
+
+
+
 app.exec_()
 sys.exit()
