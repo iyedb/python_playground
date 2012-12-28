c4f7892cf4100072e99922d18113138a74522a63
iyed <iyed.bennour@gmail.com>
2012-12-28 20:15:18 +0100

    modified the template and the html page parsing

--- mainwindow.py
+++ mainwindow.py
@@ -28,6 +28,7 @@ class MainWindow(QMainWindow):
 		self.view.setHtml( self.generate_html() )
 
 	def generate_html(self):
+		"""
 		doc = urllib2.urlopen('http://news.ycombinator.com/')
 		unicode_string = doc.read().decode('utf-8')
 		e = etree.fromstring(unicode_string, etree.HTMLParser())
@@ -47,6 +48,71 @@ class MainWindow(QMainWindow):
 		template_file = QUrl.fromLocalFile('template.html')	
 		template = Template( filename=template_file.path() )	
 		return template.render_unicode(rows=urls)
+		"""
+
+		doc = urllib2.urlopen('http://news.ycombinator.com/')
+		unicode_string = doc.read().decode('utf-8')
+		e = etree.fromstring(unicode_string, etree.HTMLParser())
+
+		def process_subtexttr(tr_tag):
+
+			strings = []
+			for i in tr_tag.itertext():
+				strings.append(i.decode('utf-8'))
+
+			entry = dict()
+		  	if len(strings) == 5:
+				entry['timestamp'] = strings[-2].strip(' |')
+			else:
+				entry['timestamp'] = strings[-1].strip()
+
+			anchors = tr_tag.xpath('td/a')
+			if len(anchors) == 2:
+				entry['comments_count'] = anchors[-1].text.decode('utf-8')
+				entry['comments_url'] = anchors[-1].get('href').decode('utf-8')
+			return entry
+
+		entries = []
+
+		for i in e.xpath('//td/a'):
+			if len(i.getparent().keys()) == 1 and i.getparent().values()[0] == 'title':
+				if isinstance(i.text, str):
+					u_s = i.text.decode('utf-8')
+				else:
+					u_s = i.text
+
+				if u_s == u'More':
+					continue
+
+				entry = dict()
+
+				entry['site'] = u''
+				entry['comments_url'] = u''
+				entry['comments_count'] = u''		    
+				entry['story_link'] = u_s
+				entry['story_url'] = i.get('href').decode('utf-8')
+
+				span_tag = i.getnext() #may be None if 'Ask HN'
+				if not span_tag is None:
+					entry['site'] = span_tag.text.decode('utf-8').strip()		     
+
+				tr_subtext = i.getparent().getparent().getnext()
+
+				if not tr_subtext is None:
+					d = process_subtexttr(tr_subtext)
+
+				entry['timestamp'] = d['timestamp']
+
+				if 'comments_url' in d:
+					entry['comments_url']  = d['comments_url']
+				if 'comments_count' in d:
+					entry['comments_count']  = d['comments_count']
+
+				entries.append(entry)
+
+		template_file = QUrl.fromLocalFile('template.html')	
+		template = Template( filename=template_file.path() )	
+		return template.render_unicode(rows=entries)
 
 
 
